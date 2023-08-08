bc4e0c360677394dc21792b0953a5626505d22bf
kdswle <kanta_shouno@dwango.co.jp>
2023-08-08 18:15:24 +0900

    名前が空のときのエラーハンドリングの追加

--- greetings_test.go
+++ greetings_test.go
@@ -8,8 +8,16 @@ import (
 func TestHelloName(t *testing.T) {
 	name := "hogeo"
 	want := fmt.Sprintf("Hi, %s. Welcome (^Д^) // !!", name)
-	msg := Hello(name)
+	msg, _ := Hello(name)
 	if msg != want {
 		t.Fatalf(`Hello("Gladys") = %q, want match for %q, nil`, msg, want)
+	}
+}
+func TestHelloNameEmpty(t *testing.T) {
+	name := ""
+
+	_, error := Hello(name)
+	if error == nil {
+		t.Fatal(`name is empty, error must be not null`)
 	}
 }
